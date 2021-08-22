bc46f0f5f2cc7b4b67e14a66dd97672babe0cde5
Lama3L9R <Guzhipei@outlook.com>
2021-08-22 23:46:06 +0800

    完成（确信 | 更新README.md | 迁移我的组件到一个文件中 | 更新README.MD

--- src/custom-components.tsx
+++ src/custom-components.tsx
@@ -1,1 +1,34 @@
-export {}
+import { Button, Typography, withStyles } from "@material-ui/core";
+
+export const GreenButton = withStyles({
+    root: {
+        color: "#4caf50",
+        border: "1px solid #4caf50"
+    }
+})(Button)
+
+export const RedButton = withStyles({
+    root: {
+        color: "#f44336",
+        border: "1px solid #f44336"
+    }
+})(Button)
+
+export const PrimaryButton = withStyles({
+    root: {
+        color: "#42a5f5",
+        border: "1px solid #42a5f5"
+    }
+})(Button)
+
+export const GrayText = withStyles({
+    root: {
+        color: "#494949"
+    }
+})(Typography)
+
+export const MoreGrayText = withStyles({
+    root: {
+        color: "#ffffff"
+    }
+})(Typography)
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
-import React, {Dispatch, SetStateAction, useRef} from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import {
-    Box,
-    Button,
-    createTheme,
-    CssBaseline, Divider,
-    FormControl, FormControlLabel,
-    Grid, Grow, IconButton, InputLabel, List, ListItem, ListItemSecondaryAction, ListItemText,
-    MenuItem,
-    Select, Switch, TextField,
-    ThemeProvider, Typography, withStyles
+    Box, Button, createTheme, CssBaseline, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider,
+    FormControl, Grid, InputLabel, List, ListItem, ListItemSecondaryAction, ListItemText, MenuItem, Select, TextField, ThemeProvider
 } from "@material-ui/core";
-import {Enchantment, enchantment, EnchantmentItem} from "./enchantment";
+import { Enchantment, enchantment, EnchantmentItem, findBestEnchantPath } from "./enchantment";
+import {GrayText, GreenButton, MoreGrayText, PrimaryButton, RedButton} from "./custom-components";
 
 const theme = createTheme({
     palette: {
@@ -33,47 +27,9 @@ const theme = createTheme({
     }
 })
 
-const GreenButton = withStyles({
-    root: {
-        color: "#4caf50",
-        border: "1px solid #4caf50"
-    }
-})(Button)
-
-const RedButton = withStyles({
-    root: {
-        color: "#f44336",
-        border: "1px solid #f44336"
-    }
-})(Button)
-
-const GrayText = withStyles({
-    root: {
-        color: "#494949"
-    }
-})(Typography)
-
 function InputRoot() {
-    const [currentItemEnchantments, setCurrentItemEnchantments] = React.useState<EnchantmentItem[]>([])
-
-    return <Box>
-        <Grid container direction="column">
-
-            <Grid item>
-                <InputArea items={currentItemEnchantments} setItems={setCurrentItemEnchantments} />
-            </Grid>
-
-            <Grid item>
-                <InputList items={currentItemEnchantments} setItems={setCurrentItemEnchantments} />
-            </Grid>
-        </Grid>
-    </Box>
-}
-
-function InputArea(child: any, items: EnchantmentItem[], setItems: Dispatch<SetStateAction<EnchantmentItem[]>>) {
+    const [items, setItems] = React.useState<EnchantmentItem[]>([])
     const [selectedEnchantment, setSelectedEnchantment] = React.useState(0)
-    const [isItem, setIsItem] = React.useState(false)
-    //const [itemLabel, setItemLabel] = React.useState("附魔书")
     const [currentEnchantments, setCurrentEnchantments] = React.useState<Enchantment[]>([])
     const createMinMaxPair = (id: number) => {
         let emt = enchantment[id]
@@ -87,106 +43,191 @@ function InputArea(child: any, items: EnchantmentItem[], setItems: Dispatch<SetS
     }
     const inputRef = React.useRef<HTMLInputElement>(null)
 
-    console.log(items)
+    const [enchantmentViewSelected, setEnchantmentViewSelected] = React.useState<EnchantmentItem | null>(null)
+    const [enchantmentViewOpen, setEnchantmentViewOpen] = React.useState(false)
 
-    return <Grid container direction="column" wrap="nowrap" spacing={2}>
-        <Grid item container direction="row" wrap="nowrap" justifyContent="space-between" alignItems="center" spacing={3}>
+    const [lazy, setLazy] = React.useState(false)
+
+    return <Box>
+        <Grid container direction="column" spacing={3}>
             <Grid item>
-                <FormControl >
-                    <InputLabel id="input-area-select-label" color="primary"> 附魔 </InputLabel>
-                    <Select labelId="input-area-select-label" defaultValue={ 0 } value={ selectedEnchantment } onChange={(e: React.ChangeEvent<{ value: unknown }>) => {
-                        setSelectedEnchantment(e.target.value as number)
-                        if(inputRef.current) {
-                            inputRef.current.value = "1"
-                        }
-                    }}>
-                        {
-                            enchantment.map((it) => {
-                                return <MenuItem value={it.id}>{ it.displayName }</MenuItem>
-                            })
-                        }
-                    </Select>
-                </FormControl>
+                <Grid container direction="column" wrap="nowrap" spacing={2}>
+                    <Grid item container direction="row" wrap="nowrap" justifyContent="space-between" alignItems="center" spacing={3}>
+                        <Grid item>
+                            <FormControl >
+                                <InputLabel id="input-area-select-label" color="primary"> 附魔 </InputLabel>
+                                <Select labelId="input-area-select-label" defaultValue={ 0 } value={ selectedEnchantment } onChange={(e: React.ChangeEvent<{ value: unknown }>) => {
+                                    setSelectedEnchantment(e.target.value as number)
+                                    if(inputRef.current) {
+                                        inputRef.current.value = "1"
+                                    }
+                                }}>
+                                    {
+                                        enchantment.map((it) => {
+                                            return <MenuItem value={it.id}>{ it.displayName }</MenuItem>
+                                        })
+                                    }
+                                </Select>
+                            </FormControl>
 
-                <TextField id="input-area-level" label="等级" type="number" defaultValue={ 1 } InputProps={createMinMaxPair(selectedEnchantment)} style={{ paddingLeft: theme.spacing(1), paddingRight: theme.spacing(1) }} color="secondary" onChange={(event) => {
-                    if(+event.target.value > enchantment[selectedEnchantment].maxLevel) {
-                        event.target.value = `${enchantment[selectedEnchantment].maxLevel}`
-                    }
+                            <TextField id="input-area-level" label="等级" type="number" defaultValue={ 1 } InputProps={createMinMaxPair(selectedEnchantment)} style={{ paddingLeft: theme.spacing(1), paddingRight: theme.spacing(1) }} color="secondary" onChange={(event) => {
+                                if(+event.target.value > enchantment[selectedEnchantment].maxLevel) {
+                                    event.target.value = `${enchantment[selectedEnchantment].maxLevel}`
+                                }
 
-                    if(+event.target.value < 1) {
-                        event.target.value = "1"
-                    }
-                }} />
-            </Grid>
+                                if(+event.target.value < 1) {
+                                    event.target.value = "1"
+                                }
+                            }} />
+                        </Grid>
 
-            <Grid item>
-                <Button color="primary" variant="outlined" onClick={(e) => {
-                    if(inputRef.current) {
-                        setCurrentEnchantments([new Enchantment(selectedEnchantment, Number(inputRef.current.value)), ...currentEnchantments])
-                    }
-                }}> 添加附魔 </Button>
-            </Grid>
-        </Grid>
-        <Divider />
-        <List>
-            { (() => {
-                if(currentEnchantments.length === 0) {
-                    return <Grid container justifyContent="center" alignItems="center">
-                        <Grid item> <GrayText> 这个物品没有任何附魔 </GrayText> </Grid>
+                        <Grid item>
+                            <Button color="primary" variant="outlined" onClick={(e) => {
+                                if(inputRef.current) {
+                                    setCurrentEnchantments([new Enchantment(selectedEnchantment, Number(inputRef.current.value)), ...currentEnchantments])
+                                }
+                            }}> 添加附魔 </Button>
+                        </Grid>
                     </Grid>
-                } else {
-                    return currentEnchantments.map((it) => {
-                        return <ListItem>
-                            <ListItemText primary={`${ it.type.displayName } ${ it.level }`} />
-                            <ListItemSecondaryAction>
-                                <RedButton onClick={(e) => {
-                                    setCurrentEnchantments(currentEnchantments.filter((e) => { return e !== it }))
-                                }}> 删除这个附魔 </RedButton>
-                            </ListItemSecondaryAction>
-                        </ListItem>
-                    })
-                }
-            })() }
-        </List>
-        <Divider />
+                    <Divider />
+                    <List>
+                        {
+                            (() => {
+                                if(currentEnchantments.length === 0) {
+                                    return <Grid container justifyContent="center" alignItems="center">
+                                        <Grid item> <GrayText> 这个物品没有任何附魔 </GrayText> </Grid>
+                                    </Grid>
+                                } else {
+                                    return currentEnchantments.map((it) => {
+                                        return <ListItem>
+                                            <ListItemText primary={`${ it.type.displayName } ${ it.level }`} />
+                                            <ListItemSecondaryAction>
+                                                <RedButton onClick={(e) => {
+                                                    setCurrentEnchantments(currentEnchantments.filter((e) => { return e !== it }))
+                                                }}> 删除这个附魔 </RedButton>
+                                            </ListItemSecondaryAction>
+                                        </ListItem>
+                                    })
+                                }
+                            })()
+                        }
+                    </List>
+                    <Divider />
 
-        <Grid item container justifyContent="space-between" alignItems="center" spacing={2}>
-            <Grid item>
-                <GreenButton variant="outlined" onClick={(e) => {
-                    setItems(items.concat([new EnchantmentItem(undefined, 0, true, ...currentEnchantments)]))
-                    setCurrentEnchantments([])
-                }}> 添加为物品 </GreenButton>
+                    <Grid item container justifyContent="space-between" alignItems="center" spacing={2}>
+                        <Grid item>
+                            <GreenButton variant="outlined" onClick={(e) => {
+                                setItems(items.concat([new EnchantmentItem(undefined, 0, true, ...currentEnchantments)]))
+                                setCurrentEnchantments([])
+                            }}> 添加为物品 </GreenButton>
+                        </Grid>
+
+                        <Grid item>
+                            <GreenButton variant="outlined" onClick={(e) => {
+                                setItems(items.concat([new EnchantmentItem(undefined, 0, false, ...currentEnchantments)]))
+                                setCurrentEnchantments([])
+                            }}> 添加为附魔书 </GreenButton>
+                        </Grid>
+                    </Grid>
+                    <RedButton variant="outlined" onClick={(e) => {
+                        setCurrentEnchantments([])
+                    }}> 清空全部附魔 </RedButton>
+
+                </Grid>
             </Grid>
 
-            <Grid item>
-                <GreenButton variant="outlined" onClick={(e) => {
-                    setItems(items.concat([new EnchantmentItem(undefined, 0, false, ...currentEnchantments)]))
-                    setCurrentEnchantments([])
-                }}> 添加为附魔书 </GreenButton>
+            <Grid item container direction="column" wrap="nowrap" spacing={2}>
+                <Grid item>
+                    <Divider />
+                    <List>
+                        {
+                            (() => {
+                                if(items.length === 0) {
+                                    return <Grid container justifyContent="center" alignItems="center">
+                                        <Grid item> <GrayText> 没有任何物品 </GrayText> </Grid>
+                                    </Grid>
+                                } else {
+                                    return items.map((it, index) => {
+                                        return <ListItem>
+                                            <ListItemText primary={`${it.isItem ? "物品" : "附魔书"} #${ index }`} onClick={(e) => {
+                                                setEnchantmentViewSelected(it)
+                                                setEnchantmentViewOpen(true)
+                                            }}/>
+                                            <ListItemSecondaryAction>
+                                                <RedButton onClick={(e) => {
+                                                    setItems(items.filter((e) => { return e !== it }))
+                                                }}> 删除这个物品 </RedButton>
+                                            </ListItemSecondaryAction>
+                                        </ListItem>
+                                    })
+                                }
+                            })()
+                        }
+                    </List>
+                    <Divider />
+                </Grid>
+                <PrimaryButton onClick={(e) => {
+                    try {
+                        console.log(findBestEnchantPath(items, undefined))
+                    } catch (e) {
+                        alert("哥，别浪费你电脑算力了，就一种方案。")
+                    }
+                    setLazy(true)
+                }}> 查找最优方案 </PrimaryButton>
+                <Dialog open={enchantmentViewOpen}>
+                    <DialogTitle>附魔</DialogTitle>
+                    <DialogContent>
+                        {
+                            (() => {
+                                if(enchantmentViewSelected?.enchantments.length === 0) {
+                                    return <DialogContentText> <MoreGrayText> 没有任何附魔 </MoreGrayText> </DialogContentText>
+                                } else {
+                                    return enchantmentViewSelected?.enchantments.map((it) => {
+                                        return <DialogContentText> { `${it.type.displayName} ${it.level}` } </DialogContentText>
+                                    })
+                                }
+                            })()
+                        }
+                    </DialogContent>
+
+                    <DialogActions>
+                        <RedButton onClick={(e) => {
+                            setEnchantmentViewOpen(false)
+                        }}>
+                            关闭
+                        </RedButton>
+                    </DialogActions>
+                </Dialog>
+
+                <Dialog open={lazy}>
+                    <DialogTitle> 教程: 如何查看结果 </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            <code>
+                                首先，拿出你的键盘。之后在你的键盘上找到一个叫做 'F12' 的按键并按下去，按下去的同时确保你的浏览器是在焦点的，如果不清楚是否在焦点，请点击网页窗口获取焦点。
+                                接下来会打开一个叫做 'Developers Tools' 的侧栏。在这个侧栏中找到 'Console' 并点下去。最后一条输出既是结果。
+                            </code>
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <GreenButton onClick={(e) => { setLazy(false) }}> 学会了 </GreenButton>
+                        <RedButton onClick={(e) => { alert("脑残滚") }}> 我还不太明白... </RedButton>
+                    </DialogActions>
+                </Dialog>
             </Grid>
         </Grid>
-        <RedButton variant="outlined" onClick={(e) => {
-            setCurrentEnchantments([])
-        }}> 清空全部附魔 </RedButton>
-
-    </Grid>
-}
-
-function InputList(child: any, items: EnchantmentItem[], setItems: Dispatch<SetStateAction<EnchantmentItem[]>>) {
-    return <div>
-
-    </div>
+    </Box>
 }
 
 function OutputRoot() {
     return <div>
-
+        { /* 咕咕咕 */ }
     </div>
 }
 
 function Foot() {
     return <div>
-
+        { /* 咕咕咕 */ }
     </div>
 }
 
